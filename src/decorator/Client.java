62e5a546ba2a4709c97ae5b82ee9347aeb0c927b
Uio <406453373@qq.com>
2021-02-15 00:32:42 +0800

    装饰器模式

---
+++ src/decorator/Client.java
@@ -0,0 +1,16 @@
+package decorator;
+
+public class Client {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.move();
+
+        System.out.println("增加新的功能，飞行-------");
+        FlyCar flyCar = new FlyCar(car);
+        flyCar.move();
+
+        System.out.println("增加新功能，水上游-------");
+        WaterCar waterCar = new WaterCar(flyCar);
+        waterCar.move();
+    }
+}
---
+++ src/decorator/ICar.java
@@ -0,0 +1,64 @@
+package decorator;
+
+public interface ICar {
+    void move();
+}
+
+class Car implements ICar{
+
+    @Override
+    public void move() {
+        System.out.println("陆地上跑");
+    }
+}
+
+// Decorator装饰角色
+class SuperCar implements ICar {
+    private ICar car;
+
+    public SuperCar(ICar car) {
+        super();
+        this.car = car;
+    }
+
+    @Override
+    public void move() {
+        car.move();
+    }
+}
+
+// 具体装饰角色
+class FlyCar extends SuperCar {
+
+    public FlyCar(ICar car) {
+        super(car);
+    }
+
+    public void fly() {
+        System.out.println("天上飞");
+    }
+
+    @Override
+    public void move() {
+        super.move();
+        fly();
+    }
+}
+
+// 具体装饰角色
+class WaterCar extends SuperCar {
+
+    public WaterCar(ICar car) {
+        super(car);
+    }
+
+    public void swim() {
+        System.out.println("水上游");
+    }
+
+    @Override
+    public void move() {
+        super.move();
+        swim();
+    }
+}
